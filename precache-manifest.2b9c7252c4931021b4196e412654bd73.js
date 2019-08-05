42aece5e2926c1143e06d1c5592f60dfad5924cb
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-08-05 02:47:05 -0400

    Updates

---
+++ precache-manifest.2b9c7252c4931021b4196e412654bd73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2bcbc3a3a4a7c6e9c85fd7da712889",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/my-app/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "4d10ddfe7da7fe9a35d4",
+    "url": "/my-app/static/js/main.8aa7b057.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
